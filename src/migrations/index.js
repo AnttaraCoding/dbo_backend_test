74fc885e961eab993ad32f2aa495fe7695f9afee
antarracoding <88897139+AnttaraCoding@users.noreply.github.com>
2022-01-06 23:24:02 +0700

    mantap

--- src/migrations/index.js
+++ src/migrations/index.js
@@ -2,20 +2,10 @@ const customer = require('./table_customer.js');
 const user = require('./table_user');
 const order = require('./table_order');
 
-
-const config = require('../config/config');
-const mysql = require('mysql2/promise');
-
-console.log(config.dbconfig)
-
 (async () => {    
     await customer()     
     await user()
     await order()
-
-    let query = 'SET FOREIGN_KEY_CHECKS=0'
-    const pool = mysql.createPool(config.dbconfig);
-    await pool.execute(query);
 
     console.log(`Migration success`)
     return
